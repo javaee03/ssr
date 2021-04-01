1e589c936240cc71d036360fa89e86170a3d61dd
javaee03 <2235850146@qq.com>
2021-04-01 17:06:26 +0800

    4-01提交

---
+++ src/main/java/com/example/srr/SrrApplication.java
@@ -0,0 +1,13 @@
+package com.example.srr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/srr/SrrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.srr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
